9cb2fcbdc852fc9079c9c47e372c531a1c64e8e4
vivianpham8 <52727841+vivianpham8@users.noreply.github.com>
2020-04-25 15:31:38 -0400

    upvote/downvote

--- uvafooddeals.php
+++ uvafooddeals.php
@@ -1,6 +1,7 @@
 <?php
     include('navbar.html');
     include("uvafooddeals.html");
+    ob_start();
 ?>
 
 <?php
@@ -41,11 +42,25 @@
                         <form action='uvafooddeals.php' method='post'>
                             <button class='btn btn-primary' type='submit' name='" . $eventID . "upvote'>Upvote</button>
                             <button class='btn btn-danger' type='submit' name='" . $eventID . "downvote'>Downvote</button>
+                            <input type='hidden' id='eventID' name='eventID' value=$eventID>
+                            <input type='hidden' id='userID' name='userID' value=$userID>
                         </form>
                     </div>
                   </div>
             ";
             echo "</div>";
+
+            // if upvote button clicked, call upvote function
+            if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST[$eventID . 'upvote']))
+            {
+                upvote();
+            }
+
+            // if downvote button clicked, call downvote function
+            if($_SERVER['REQUEST_METHOD'] == "POST" and isset($_POST[$eventID . 'downvote']))
+            {
+                downvote();
+            }
         }
     }
     
@@ -61,4 +76,113 @@
             // echo "userText: " . $result['userText'] . "numStars: " . $result['numStars'] . "<br/>";
     }
     */
+?>
+
+<?php
+    function upvote()
+    {
+        global $db;
+        $eventID = $_POST['eventID'];
+        $userID = $_POST['userID'];
+        $query = "SELECT * FROM votes WHERE eventID = :eventID AND userID = :userID";
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':eventID', $eventID);
+        $statement->bindValue(':userID', $userID);
+        $statement->execute();
+
+        $results = $statement->fetch();
+        $statement->closeCursor();
+
+        if($statement->rowCount() > 0) { // if user already voted on event
+           if($results['upvote'] != 1) { // downvoted but want to change to upvote
+               $query = "UPDATE votes SET upvote = 1 WHERE eventID = :eventID AND userID = :userID";
+               $statement = $db->prepare($query);
+               $statement->bindValue(':eventID', $eventID);
+               $statement->bindValue(':userID', $userID);
+               $statement->execute();
+
+               $query = "UPDATE votes SET downvote = 0 WHERE eventID = :eventID AND userID = :userID";
+               $statement = $db->prepare($query);
+               $statement->bindValue(':eventID', $eventID);
+               $statement->bindValue(':userID', $userID);
+               $statement->execute();
+
+               $query = "UPDATE event SET total_votes = total_votes + 1 WHERE eventID = :eventID";
+               $statement = $db->prepare($query);
+               $statement->bindValue(':eventID', $eventID);
+               $statement->execute();
+               $statement->closeCursor();
+           }
+        }
+        else{
+            $query = "INSERT INTO votes VALUES(:eventID, :userID, 1, 0)";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':eventID', $eventID);
+            $statement->bindValue(':userID', $userID);
+
+            $statement->execute();
+            $statement->closeCursor();
+
+            $query = "UPDATE event SET total_votes = total_votes + 1 WHERE eventID = :eventID";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':eventID', $eventID);
+            $statement->execute();
+            $statement->closeCursor();
+        }
+        header('Location: uvafooddeals.php'); // refresh after updating vote
+    }
+
+    function downvote()
+    {
+        global $db;
+        $eventID = $_POST['eventID'];
+        $userID = $_POST['userID'];
+        $query = "SELECT * FROM votes WHERE eventID = :eventID AND userID = :userID";
+
+        $statement = $db->prepare($query);
+        $statement->bindValue(':eventID', $eventID);
+        $statement->bindValue(':userID', $userID);
+        $statement->execute();
+
+        $results = $statement->fetch();
+        $statement->closeCursor();
+        if($statement->rowCount() > 0) { // if user already voted on event
+            if($results['downvote'] != 1) { // upvoted but want to change to downvote
+                $query = "UPDATE votes SET upvote = 0 WHERE eventID = :eventID AND userID = :userID";
+                $statement = $db->prepare($query);
+                $statement->bindValue(':eventID', $eventID);
+                $statement->bindValue(':userID', $userID);
+                $statement->execute();
+
+                $query = "UPDATE votes SET downvote = 1 WHERE eventID = :eventID AND userID = :userID";
+                $statement = $db->prepare($query);
+                $statement->bindValue(':eventID', $eventID);
+                $statement->bindValue(':userID', $userID);
+                $statement->execute();
+
+                $query = "UPDATE event SET total_votes = total_votes - 1 WHERE eventID = :eventID";
+                $statement = $db->prepare($query);
+                $statement->bindValue(':eventID', $eventID);
+                $statement->execute();
+                $statement->closeCursor();
+            }
+        }
+        else{
+            $query = "INSERT INTO votes VALUES(:eventID, :userID, 0, 1)";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':eventID', $eventID);
+            $statement->bindValue(':userID', $userID);
+
+            $statement->execute();
+            $statement->closeCursor();
+
+            $query = "UPDATE event SET total_votes = total_votes - 1 WHERE eventID = :eventID";
+            $statement = $db->prepare($query);
+            $statement->bindValue(':eventID', $eventID);
+            $statement->execute();
+            $statement->closeCursor();
+        }
+        header('Location: uvafooddeals.php'); // refresh after updating vote
+    }
 ?>
